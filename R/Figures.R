fc24fea2f08ef8d0ec5b8512699f0e715e28d771
Kenny Wong <Wong.Kenny@epa.gov>
2024-06-26 13:02:04 -0400

    Modified legend ifelse
    did not want to change current plotly legend name argument of its current logic. So inserted and ifelse that should hopefully keep same logic if id_col does not equal to the characteristic name for comparabledataidentifier.

--- R/Figures.R
+++ R/Figures.R
@@ -908,7 +908,7 @@ TADA_TwoCharacteristicScatterplot <- function(.data, id_cols = "TADA.ComparableD
   # create TADA color palette
   tada.pal <- TADA_ColorPalette()
 
-  scatterplot <- plotly::plot_ly(type = "scatter", mode = "markers") %>%
+  scatterplot <- plotly::plot_ly(type = "scatter", mode = "markers", name = c(groups[1], groups[1], groups[2])) %>%
     plotly::layout(
       xaxis = list(
         # title = "Activity Start Date", # not necessary?
